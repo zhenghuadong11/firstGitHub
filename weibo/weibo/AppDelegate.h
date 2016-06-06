a4c4c192ee2baee414173e76ec428d2471e0193f
zhenghuadong <zhenghuadong@zhenghuadongdemacbook-pro.local>
2016-06-06 22:07:32 +0800

    第一个

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by zhenghuadong on 16/6/6.
+//  Copyright © 2016年 zhenghuadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
